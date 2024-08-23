78d1dc8905faa2fbfddedc6afc5da3d882a8bb9a
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-23 23:29:37 +0900

    :+1: unify home button

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import "../app.postcss";
-  import { AppBar } from "@skeletonlabs/skeleton";
   import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
   initializeStores();
 
@@ -9,6 +8,7 @@
   import { storePopup } from "@skeletonlabs/skeleton";
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
+  import Icon from "@iconify/svelte";
   import { goto } from "$app/navigation";
 </script>
 
@@ -20,14 +20,19 @@
 <Toast position="tr" rounded="rounded-lg" />
 
 <div class="flex flex-col h-screen">
-  <div class="border-b border-gray-400">
-    <AppBar class="!p-1 md:!p-2">
-      <div class="flex items-center h-full">
-        <a href="/" class="flex items-center" on:click|preventDefault={() => goto("/")}>
-          <div class="text-sm md:text-lg">TOP</div>
-        </a>
-      </div>
-    </AppBar>
+  <div class="border-b border-gray-400 bg-gray-100">
+    <div class="flex items-center justify-between h-full">
+      <a
+        href="/"
+        class="flex flex-row items-center space-x-1 pt-1 pb-1 pl-1 pr-2 m-1 text-sm text-gray-500 bg-white border border-gray-400 rounded-md"
+        on:click|preventDefault={() => goto("/")}
+      >
+        <div class="w-5 h-5">
+          <Icon icon="mdi:home-outline" class="text-gray-500 w-full h-full" />
+        </div>
+        <span class="">HOME</span>
+      </a>
+    </div>
   </div>
 
   <div class="container mx-auto overflow-y-auto pb-16">
