de5c285762fa761d34c5cd4336bf4d7e58f9769c
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-23 23:26:32 +0900

    :shower: remove navigateTo

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -9,7 +9,7 @@
   import { storePopup } from "@skeletonlabs/skeleton";
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-  import { navigateTo } from "$lib/utils/navigation.client";
+  import { goto } from "$app/navigation";
 </script>
 
 <svelte:head>
@@ -23,7 +23,7 @@
   <div class="border-b border-gray-400">
     <AppBar class="!p-1 md:!p-2">
       <div class="flex items-center h-full">
-        <a href="/" class="flex items-center" on:click|preventDefault={() => navigateTo("/")}>
+        <a href="/" class="flex items-center" on:click|preventDefault={() => goto("/")}>
           <div class="text-sm md:text-lg">TOP</div>
         </a>
       </div>
