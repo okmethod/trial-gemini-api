a33b6038e5f5c85e508d828b8c887cef2e6d588b
shohei.okugi <shohei.okugi@ghelia.com>
2024-08-24 11:39:40 +0900

    :+1: redirect to PokeAPI Stadium

--- skeleton-app/src/lib/constants/common.ts
+++ skeleton-app/src/lib/constants/common.ts
@@ -1,1 +1,2 @@
 export const GITHUB_REPO_URL = "https://github.com/okmethod/trial-gemini-api";
+export const POKE_API_STADIUM_URL = "https://okmethod.github.io/poke-api-stadium/";
--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -9,7 +9,7 @@
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
   import Icon from "@iconify/svelte";
-  import { goto } from "$app/navigation";
+  import { POKE_API_STADIUM_URL } from "$lib/constants/common";
 </script>
 
 <svelte:head>
@@ -23,9 +23,8 @@
   <div class="border-b border-gray-400 bg-gray-100">
     <div class="flex items-center justify-between h-full">
       <a
-        href="/"
+        href={POKE_API_STADIUM_URL}
         class="flex flex-row items-center space-x-1 pt-1 pb-1 pl-1 pr-2 m-1 text-sm text-gray-500 bg-white border border-gray-400 rounded-md"
-        on:click|preventDefault={() => goto("/")}
       >
         <div class="w-5 h-5">
           <Icon icon="mdi:home-outline" class="text-gray-500 w-full h-full" />
