44572e400a2214936747d828b79365e07cdd7997
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-10 13:38:26 +0900

    :shower: remove text-gray

--- skeleton-app/src/routes/+error.svelte
+++ skeleton-app/src/routes/+error.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex h-screen">
   <div class="m-auto text-center">
-    <p class="text-4xl font-bold text-gray-800 leading-loose tracking-wider">
+    <p class="text-4xl font-bold leading-loose tracking-wider">
       {$page.status}
       {$page.error?.message}
     </p>
