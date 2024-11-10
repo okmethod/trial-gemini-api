8103907f43ff6cd2b511261c0d4731acdecf0d35
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-10 10:50:00 +0900

    :+1: apply theme

--- skeleton-app/src/routes/+layout.svelte
+++ skeleton-app/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import { Toast, Modal, initializeStores, storePopup } from "@skeletonlabs/skeleton";
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
   import Icon from "@iconify/svelte";
+  import { setTheme } from "$lib/stores/theme";
   import { getAudioOn, setAudioOn } from "$lib/stores/audio";
   import { generations, getGenerationId, type GenerationId } from "$lib/stores/generation";
   import { pickRandomNumbers } from "$lib/utils/collections";
@@ -22,6 +23,7 @@
   initializeStores();
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
+  let isLoaded = false;
   let currentAudioOn = false;
   let currentGenerationId: GenerationId | null = null;
   let currentGenerationImageUrl: string | null = null;
@@ -32,7 +34,12 @@
     options = options.filter((option) => option.value !== "");
     const audio = document.createElement("audio");
     const isSupportedOgg = !!audio.canPlayType('audio/ogg; codecs="vorbis"');
-    await loadFFmpeg(isSupportedOgg);
+
+    function wait(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    await Promise.all([setTheme({ name: "hamlindigo", dark: true }), loadFFmpeg(isSupportedOgg), wait(500)]);
+    isLoaded = true;
   });
 
   function getSymbolImageUrl(generationId: GenerationId): string {
@@ -53,44 +60,51 @@
 <Modal />
 <Toast position="tr" rounded="rounded-lg" />
 
-<div class="flex flex-col h-screen">
-  <div class="relative border-b border-gray-400 bg-gray-100">
-    <div class="flex items-center justify-between h-full">
-      <a
-        href={POKE_API_STADIUM_URL}
-        class="flex flex-row items-center space-x-1 pt-1 pb-1 pl-1 pr-2 m-1 text-sm text-gray-500 bg-white border border-gray-400 rounded-md"
-      >
-        <div class="w-5 h-5">
-          <Icon icon="mdi:home-outline" class="text-gray-500 w-full h-full" />
+{#if isLoaded}
+  <div class="flex flex-col h-screen">
+    <div class="relative border-b border-gray-400 bg-gray-100">
+      <div class="flex items-center justify-between h-full">
+        <a
+          href={POKE_API_STADIUM_URL}
+          class="flex flex-row items-center space-x-1 pt-1 pb-1 pl-1 pr-2 m-1 text-sm text-gray-500 bg-white border border-gray-400 rounded-md"
+        >
+          <div class="w-5 h-5">
+            <Icon icon="mdi:home-outline" class="text-gray-500 w-full h-full" />
+          </div>
+          <span class="">HOME</span>
+        </a>
+        <div class="w-8 h-8 bg-white border border-gray-400 rounded-full ml-1">
+          <button on:click={toggleAudioOn} class="w-full h-full flex items-center justify-center">
+            <Icon icon={currentAudioOn ? "mdi:volume-high" : "mdi:volume-off"} class="text-gray-500 w-3/4 h-3/4" />
+          </button>
         </div>
-        <span class="">HOME</span>
-      </a>
-      <div class="w-8 h-8 bg-white border border-gray-400 rounded-full ml-1">
-        <button on:click={toggleAudioOn} class="w-full h-full flex items-center justify-center">
-          <Icon icon={currentAudioOn ? "mdi:volume-high" : "mdi:volume-off"} class="text-gray-500 w-3/4 h-3/4" />
-        </button>
+        <div class="flex-grow"><!--spacer--></div>
+        <div class="w-8 h-8 bg-white border border-gray-400 rounded-full">
+          <img
+            src={currentGenerationImageUrl}
+            alt="generationSymbol"
+            class="w-full h-full object-contain transform scale-150"
+          />
+        </div>
+        <select
+          id="generationId"
+          bind:value={currentGenerationId}
+          class="w-24 pt-1 pb-1 pl-2 pr-2 m-1 text-sm text-gray-500 border-gray-400 rounded-md"
+        >
+          {#each options as option}
+            <option value={option.value}>{option.label}</option>
+          {/each}
+        </select>
       </div>
-      <div class="flex-grow"><!--spacer--></div>
-      <div class="w-8 h-8 bg-white border border-gray-400 rounded-full">
-        <img
-          src={currentGenerationImageUrl}
-          alt="generationSymbol"
-          class="w-full h-full object-contain transform scale-150"
-        />
-      </div>
-      <select
-        id="generationId"
-        bind:value={currentGenerationId}
-        class="w-24 pt-1 pb-1 pl-2 pr-2 m-1 text-sm text-gray-500 border-gray-400 rounded-md"
-      >
-        {#each options as option}
-          <option value={option.value}>{option.label}</option>
-        {/each}
-      </select>
+    </div>
+
+    <div class="container mx-auto overflow-y-auto pb-16">
+      <slot />
     </div>
   </div>
-
-  <div class="container mx-auto overflow-y-auto pb-16">
-    <slot />
+{:else}
+  <div class="flex items-center justify-center h-screen bg-gray-200 space-x-2">
+    <div class="font-mono text-black text-2xl">Now Loading...</div>
+    <!-- <img src={data.footerSymbolUrl} alt="footerSymbol" /> -->
   </div>
-</div>
+{/if}
--- skeleton-app/tailwind.config.ts
+++ skeleton-app/tailwind.config.ts
@@ -4,6 +4,19 @@ import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
 import { skeleton } from "@skeletonlabs/tw-plugin";
 
+enum Theme {
+  Skeleton = "skeleton",
+  Wintry = "wintry",
+  Modern = "modern",
+  Rocket = "rocket",
+  Seafoam = "seafoam",
+  Vintage = "vintage",
+  Sahara = "sahara",
+  Hamlindigo = "hamlindigo",
+  GoldNouveau = "gold-nouveau",
+  Crimson = "crimson",
+}
+
 export default {
   darkMode: "class",
   content: [
@@ -13,17 +26,20 @@ export default {
   theme: {
     extend: {},
   },
+  extend: {
+    scrollbarGutter: {
+      stable: "stable",
+    },
+  },
   plugins: [
     forms,
     typography,
     skeleton({
       themes: {
-        preset: [
-          {
-            name: "rocket",
-            enhancements: true,
-          },
-        ],
+        preset: Object.values(Theme).map((theme) => ({
+          name: theme,
+          enhancements: true,
+        })),
       },
     }),
   ],
