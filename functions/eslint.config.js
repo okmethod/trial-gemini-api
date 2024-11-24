430ed3fe5f90edc5a366285723541f0e6875daac
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-24 12:25:19 +0900

    :+1: use prettier

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const js = require("@eslint/js");
 const ts = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
 const globals = require("globals");
 
 /** @type {import('eslint').Linter.Config[]} */
 module.exports = [
   js.configs.recommended,
+  prettier,
   ...ts.configs.recommended,
   {
     languageOptions: {
